b1c534897bc5ee844588b53dd38ff56997e5d3ff
Aritra Kar <aritrakar2002@gmail.com>
2021-07-14 11:38:54 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,7 @@ setup(name='statpy',
       version='0.1',
       description='Gaussian and Binomial distributions (for Udacity AWS MLE Foundations course)',
       packages=['statpy'],
+      author="Aritra Kar",
+      author_email="aritrakar2002@gmail.com",
+      keywords=['python', 'statistics', 'data-analysis'],
       zip_safe=False)
